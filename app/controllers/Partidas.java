38b3991b92ece666c29205f28f6f21ce86ebe25a
rnfanego <rnfanego@gmail.com>
2014-05-25 23:20:47 -0300

    adding routes and controllers

---
+++ app/controllers/Partidas.java
@@ -0,0 +1,19 @@
+package controllers;
+
+import play.mvc.Controller;
+import play.mvc.Result;
+
+/**
+ * @author rfanego
+ */
+public class Partidas extends Controller {
+	public static Result obtenerPartidasActivas(String idJugador) {
+		
+        return ok();
+    }
+	
+	public static Result buscar(int jugadores) {
+		
+        return ok();
+    }
+}
---
+++ app/controllers/Usuarios.java
@@ -0,0 +1,19 @@
+package controllers;
+
+import play.mvc.Controller;
+import play.mvc.Result;
+
+/**
+ * @author rfanego
+ */
+public class Usuarios extends Controller {
+	public static Result registrar() {
+		
+        return ok();
+    }
+	
+	public static Result validar() {
+		
+        return ok();
+    }
+}
