f2225697cdcf9c0f2de9df96da2104860be6736f
rnfanego <rnfanego@gmail.com>
2014-05-26 22:40:34 -0300

    adding registrar Usuario service, actually fails

--- app/controllers/Usuarios.java
+++ app/controllers/Usuarios.java
@@ -1,5 +1,15 @@
 package controllers;
 
+import java.net.UnknownHostException;
+
+import models.Jugador;
+
+import org.mongodb.morphia.Datastore;
+import org.mongodb.morphia.Morphia;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.mongodb.Mongo;
+
 import play.mvc.Controller;
 import play.mvc.Result;
 
@@ -8,8 +18,38 @@ import play.mvc.Result;
  */
 public class Usuarios extends Controller {
 	public static Result registrar() {
-		
-        return ok();
+		JsonNode json = request().body().asJson();
+		if(json == null){
+			return badRequest();
+		}else{
+			String usuario = json.get("usuario").asText();
+			String clave = json.get("clave").asText();
+			String tipo = json.get("tipo").asText();
+
+			String dbName = new String("tutti");
+			Mongo mongo = null;
+			try {
+				mongo = new Mongo( "localhost", 27017 );
+			} catch (UnknownHostException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			Morphia morphia = new Morphia();
+			Datastore datastore = morphia.createDatastore(mongo, dbName); 
+			
+			morphia.mapPackage("models");
+			
+			Jugador jugador = new Jugador();
+			jugador.setNickname(usuario);
+			jugador.setClave(clave);
+			if(tipo.equals("mail")){
+				jugador.setMail(usuario);
+			}
+			
+			datastore.save(jugador);
+
+			return ok();
+		}
     }
 	
 	public static Result validar() {
